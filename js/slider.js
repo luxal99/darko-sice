bb6118802ac7615021233e80054f7993f9be6230
Aleksa <aleksa.lukic.3659@metropolitan.ac.rs>
2021-02-17 17:41:02 +0100

    End.

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,15 @@
 import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js'
 
+let slidesPerVIew = 3;
+
+if (window.screen.width <= 570){
+    slidesPerVIew = 1
+}
+
+console.log(window.screen.width <= 570)
 const swiper = new Swiper('.swiper-container', {
     // Optional parameters
-    slidesPerView: 3,
+    slidesPerView: slidesPerVIew,
     spaceBetween: 30,
     direction: 'horizontal',
     loop: true,
